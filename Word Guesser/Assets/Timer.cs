a9795449aeb7e93abe3a5f02c7b2df2486602692
MyPing0 <myping0yt@gmail.com>
2022-02-27 15:09:36 -0500

    Made the timer blink red at 15sec

--- Word Guesser/Assets/Timer.cs
+++ Word Guesser/Assets/Timer.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     public Guess guesser;
+    private Animator animator;
+    public GameObject timerGameObject;
     private float minutes;
     private float seconds;
     public TMP_Text countDownText;
@@ -13,21 +15,42 @@ public class Timer : MonoBehaviour
     public byte[] redColor = new byte[4];
     void Start()
     {
+        //adding 1 second to the timer so that the timer doesnt immediately start counting down
         time = DifficultyManager.countDown + 1;
-        if (time == 1) countDownText.gameObject.SetActive(false);
+        if (time == 1) timerGameObject.SetActive(false);
+        animator = timerGameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame  
     void Update()
     {
-        if (time > 0)
+        if (timerGameObject.activeSelf)
         {
-            minutes = Mathf.FloorToInt(time / 60);
-            seconds = Mathf.FloorToInt(time % 60);
-            countDownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            time -= Time.deltaTime;
+            if (time > 0)
+            {
+                minutes = Mathf.FloorToInt(time / 60);
+                seconds = Mathf.FloorToInt(time % 60);
+                countDownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                time -= Time.deltaTime;
+            }
+            if (time < 15 && countDownText.color != Color.red)
+            {
+                countDownText.color = new Color32(redColor[0], redColor[1], redColor[2], redColor[3]);
+                StartCoroutine("PlayWarningAnimation");
+
+            }
+
+            if (time <= 0) guesser.ResetGame();
         }
-        if (time < 15 && countDownText.color != Color.red) countDownText.color = new Color32(redColor[0], redColor[1], redColor[2], redColor[3]);
-        if (time <= 0) guesser.ResetGame();
+    }
+    IEnumerator PlayWarningAnimation()
+    {
+        //if the "warningMessage" animation is not currently playing
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("TimeRunningOut"))
+        {
+            animator.SetBool("timerWarning", true);
+            yield return new WaitForSeconds(15);
+            animator.SetBool("timerWarning", false);
+        }
     }
 }
